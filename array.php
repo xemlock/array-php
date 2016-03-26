6aa3dc738e4c1ba99766dda17d2ac038800bfdab
xemlock <xemlock@gmail.com>
2016-03-26 17:14:55 +0100

    Added argument type check, added array_flatten

--- array.php
+++ array.php
@@ -7,8 +7,15 @@ if (!function_exists('array_first')) {
      * @param array $array
      * @return mixed
      */
-    function array_first(array $array)
+    function array_first($array)
     {
+        if (!is_array($array)) {
+            trigger_error(
+                sprintf('%s expects parameter %d to be array, %s given', __FUNCTION__, 1, gettype($array)),
+                E_USER_WARNING
+            );
+            return null;
+        }
         return reset($array);
     }
 }
@@ -20,26 +27,87 @@ if (!function_exists('array_last')) {
      * @param array $array
      * @return mixed
      */
-    function array_last(array $array)
+    function array_last($array)
     {
+        if (!is_array($array)) {
+            trigger_error(
+                sprintf('%s expects parameter %d to be array, %s given', __FUNCTION__, 1, gettype($array)),
+                E_USER_WARNING
+            );
+            return null;
+        }
         return end($array);
     }
 }
 
 if (!function_exists('array_reduce_left')) {
     /**
-     * Iteratively reduce the array to a single value using a callback function
-     * applied for each array value (from left-to-right)
+     * Reduce the array to a single value using a callback function applied
+     * for each array value (from left to right)
      *
      * @param array $array
      * @param callable $callback
      * @param mixed $initial OPTIONAL
      * @return mixed
      */
-    function array_reduce_left(array $array, $callback, $initial = null) {
+    function array_reduce_left($array, $callback, $initial = null)
+    {
+        if (!is_array($array)) {
+            trigger_error(
+                sprintf('%s expects parameter %d to be array, %s given', __FUNCTION__, 1, gettype($array)),
+                E_USER_WARNING
+            );
+            return null;
+        }
         foreach ($array as $key => $value) {
             $initial = call_user_func($callback, $initial, $value, $key);
         }
         return $initial;
+    }
+}
+
+if (!function_exists('array_flatten')) {
+    /**
+     * Convert multi-dimensional array into a flat array
+     *
+     * Arrays are traversed in the depth-first order.
+     *
+     * If the arrays have the same string keys, then the later value for that
+     * key will overwrite the previous one. If, however, the arrays contain
+     * numeric keys, the later value will not overwrite the original value,
+     * but will be appended.
+     *
+     * @param $array
+     * @return array
+     */
+    function array_flatten($array)
+    {
+        if (!is_array($array)) {
+            trigger_error(
+                sprintf('%s expects parameter %d to be array, %s given', __FUNCTION__, 1, gettype($array)),
+                E_USER_WARNING
+            );
+            return null;
+        }
+        $stack = array(array(null, $array));
+        $result = array();
+        while (count($stack)) {
+            list($key, $value) = array_shift($stack);
+            if (is_array($value)) {
+                // insert array items at the beginning of the stack maintaining
+                // their order in the original array
+                end($value);
+                while (($k = key($value)) !== null) {
+                    $v = current($value);
+                    array_unshift($stack, array($k, $v));
+                    prev($value);
+                }
+            } elseif (is_int($key)) {
+                $result[] = $value;
+            } else {
+                $result[$key] = $value;
+            }
+        }
+        return $result;
     }
 }
